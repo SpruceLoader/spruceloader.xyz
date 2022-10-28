215ed321eef4203f9488e565023755f947cb73eb
Deftu <deftudev@gmail.com>
2022-10-28 22:06:24 +0200

    Added start script.

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the current dir location
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+# Ensure we are in the correct dir
+cd $DIR
+# Rebuild the site
+npm run build
+# Start the site on port 3009
+PORT=3010 /usr/bin/node build/index.js
